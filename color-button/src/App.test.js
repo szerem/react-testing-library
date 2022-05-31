10ada17245a3adb209b533569fcb685007057384
Krzysztof <szerem@users.noreply.github.com>
2022-05-31 15:14:24 +0200

    first tests

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Change to blue/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("button has correct initial color", () => {
+  render(<App />);
+  // throw new Error("test failed");
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button turns blue when clicked", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton.textContent).toBe("Change to red");
 });
--- readme.md
+++ readme.md
@@ -2,3 +2,10 @@
 
 npm test 
 
+
+https://www.w3.org/TR/wai-aria/#role_definitions
+
+https://github.com/testing-library/jest-dom
+
+https://github.com/bonnie/udemy-testing-library/tree/main/lecture-code
+
